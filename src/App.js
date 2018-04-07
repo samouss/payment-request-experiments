02ec227c8fc73dd24265a25357161f50f70e1e2e
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-07 19:17:33 +0200

    refactor: render full App with React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { InstantSearch, Configure } from 'react-instantsearch/dom';
 import SearchBox from './components/SearchBox';
 import Hits from './components/Hits';
@@ -10,19 +10,44 @@ class App extends Component {
 
   render() {
     return (
-      <section className="section">
-        <div className="container">
-          <InstantSearch
-            appId="latency"
-            apiKey="6be0576ff61c053d5f9a3225e2a90f76"
-            indexName="instant_search"
-          >
-            <Configure hitsPerPage={8} />
-            <SearchBox />
-            <Hits onClick={this.onClick} />
-          </InstantSearch>
-        </div>
-      </section>
+      <Fragment>
+        <section className="hero is-medium is-dark is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Payment Request API</h1>
+              <h2 className="subtitle">
+                Algolia React InstantSearch + Payment Request API.
+              </h2>
+            </div>
+          </div>
+        </section>
+
+        <section className="section">
+          <div className="container">
+            <div className="notification is-danger">
+              Your browser doesn't support <a href="">PaymentRequest</a>.
+            </div>
+
+            <div className="notification">
+              This is an experiments using{' '}
+              <a href="">Algolia React InstantSearch</a> &{' '}
+              <a href="">PaymentRequest</a> APIs. You can find the code on{' '}
+              <a href="">GitHub</a>.
+            </div>
+
+            <InstantSearch
+              appId="latency"
+              apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+              indexName="instant_search"
+            >
+              <Configure hitsPerPage={5} />
+
+              <SearchBox />
+              <Hits onClick={this.onClick} />
+            </InstantSearch>
+          </div>
+        </section>
+      </Fragment>
     );
   }
 }
